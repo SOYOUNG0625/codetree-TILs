9ee0f76673ae449010fa464bff8a48dda4da9bc3
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2024-07-26 19:47:16 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    int chu=13;
+    double gra=0.165000;
+
+    cout.precision(6);
+    cout << chu << " * " << gra << " = " << chu*gra << endl;
+    return 0;
+}
